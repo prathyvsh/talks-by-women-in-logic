6ef8da26f5ecc89754814ed0cd2e637b0e26e404
Prathyush <prathyushpramod@gmail.com>
2022-11-28 18:14:25 +0000

    Fixes in data and styles

--- ui.js
+++ ui.js
@@ -1,6 +1,6 @@
 const talkGridView = (data) => {
 
-    const {author = "No Author",
+    let {author = "No Author",
             title = "No Title",
             date = "No Year",
             author_image = "default-avatar.png",
@@ -9,14 +9,21 @@ const talkGridView = (data) => {
             author_link = "#",
         description = "No description provided"} = data;
 
+        console.log(data);
+
+        if(talk_link) {
+            video_id = talk_link.split("watch?v=")[1];
+            talk_image = `https://img.youtube.com/vi/${video_id}/maxresdefault.jpg`;
+        };
+
     return ["div.card",
         ["a.video-thumbnail",
             ["img", {alt: `${title}  by ${author} given on ${date}`, src: talk_image}]],
             ["div.date-and-author",
             ["div.title-and-date",
             ["a.talk-link", {href: talk_link}, ["h3.title", title]],
-                ["time", year]],
-                ["div.author",
+                ["time", date]],
+                ["a.author", {href: author_link},
                 ["div.avatar", ["img", {src: author_image, alt: `Portrait of ${author}`}]],
                 ["p", author]]],
                 ["p.description", description]];
@@ -25,17 +32,17 @@ const talkGridView = (data) => {
 
 /* Toggle List or Grid Mode */
 
-z.$("#list-mode .list-view").addEventListener("click", () => { z.$("#talk-list").classList = "list-mode"; });
-
-z.$("#list-mode .grid-view").addEventListener("click", () => { z.$("#talk-list").classList = "grid-mode"; });
-
 const talks = (data) => data.map(talkGridView);
 
 const render = async () => {
 
+    z.$("#view-selector .list-view").addEventListener("click", () => { z.$("#talk-list").classList = "list-mode"; });
+
+    z.$("#view-selector .grid-view").addEventListener("click", () => { z.$("#talk-list").classList = "grid-mode"; });
+
     const data = await (await fetch("./data.json")).json();
 
-    z.render("#talk-list", data ? tasks(data) : ["p", "No talks found!"]);
+    z.render("#talk-list", (data != null) ? data.map(d => talkGridView(d)) : ["p", "No talks found!"]);
 
 };
 
