c95cb22616b8e0ab731611b441660ec30b2b2720
Prathyush <prathyushpramod@gmail.com>
2023-01-06 06:53:44 +0530

    Change regex for matching video talk

--- ui.js
+++ ui.js
@@ -12,7 +12,10 @@ const talkGridView = (data) => {
         console.log(data);
 
         if(talk_link) {
-            video_id = talk_link.split("watch?v=")[1];
+            
+            video_url_regex = /v=([\w\-\_]+)/;
+            video_id_matches = talk_link.match(video_url_regex);
+            video_id = video_id_matches ? video_id_matches[1] : talk_image;
             talk_image = `https://img.youtube.com/vi/${video_id}/maxresdefault.jpg`;
         };
 
